9ebf3628edb25dd9779dad28d2054cd3e563167b
Ludovic de Luna <ludovic.deluna@gmail.com>
2020-11-12 01:28:21 +0100

    clean unused code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,4 @@
 class RecipesController < ApplicationController
-  def show
-  end
-
   def search
     ingredients = (params[:ingredients] || "").split(',').map(&:strip).compact
     @recipes = RecipeFinderService.new.can_cook_with(ingredients + %w(sel poivre huile))
